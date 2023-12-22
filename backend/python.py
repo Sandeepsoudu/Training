2b66bf853a54c98e0009c04c3955de795e1734f7
Sandeepsoudu <2203a51l86@sru.edu.in>
2023-12-22 10:52:22 +0530

    changes

---
+++ backend/python.py
@@ -0,0 +1,34 @@
+"""b=[1,2,3]
+b[1]=5
+print(b)"""
+"""a={
+    "hi":"hello",
+    "year":2000
+}
+a["year"]=2001
+print(a)"""
+"""b=[1,2,3]
+print(set(b))"""
+"""def add(num1,num2):
+    print(num1+num2)
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+add(num1,num2)"""
+"""l=[1,3,5,6,8]
+i=int(input())
+if i==l:
+    print("found")
+else:
+    print("not found")"""
+"""a=float(input())
+if a<20:
+    print("cold")
+elif a>20 and a<=32:
+    print("normal")
+else:
+    print("warm")"""
+
+
+
+
+
